15a35422d7d8500be4e4cdb50c3952987bb8b6f6
Bot Server <agiorno@me.com>
2020-08-02 22:11:01 +0000

    bot file adding

---
+++ bot11.py
@@ -0,0 +1,50 @@
+# file - ~/Bot/bot.py
+# venv - ~/Bot/bot
+
+from __future__ import unicode_literals
+from flask import Flask, request
+import telegram
+from telegram.replykeyboardmarkup import ReplyKeyboardMarkup
+
+app = Flask(__name__)
+app.debug = True
+
+TOKEN = "1114545163:AAFwRXupgXrPe_UPdekL-M1AwBCqTlQbeSA"
+
+global bot
+bot = telegram.Bot(token=TOKEN)
+
+URL = '161.35.17.195'
+
+
+#WebHook
+@app.route('/HOOK', methods=['POST', 'GET']) 
+def webhook_handler():
+    if request.method == "POST": 
+        update = telegram.Update.de_json(request.get_json(force=True), bot)
+        try:
+            kb = ReplyKeyboardMarkup([["Обновить"]])
+            chat_id = update.message.chat.id 
+            text = update.message.text
+            userid = update.message.from_user.id
+	    username = update.message.from_user.username
+            bot.send_message(chat_id=chat_id, text=username, reply_markup=kb)
+        except Exception as e:
+            print(e)
+    return 'ok' 
+
+#Set_webhook 
+@app.route('/set_webhook', methods=['GET', 'POST']) 
+def set_webhook(): 
+    s = bot.setWebhook('https://%s:443/HOOK' % URL, certificate=open('/etc/ssl/istel/server.crt', 'rb')) 
+    if s:
+        print(s)
+        return "webhook setup ok" 
+    else: 
+        return "webhook setup failed" 
+
+@app.route('/') 
+def index(): 
+    return '<h1>HEY</h1>' 
+
+
